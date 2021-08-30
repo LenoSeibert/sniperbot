f2f0182cb5f71c695a70e1e8aec28bf02168dde0
Lenoir Seibert <siwebbr@gmail.com>
2021-08-30 11:09:43 -0300

    dia 1

---
+++ docs.md
@@ -0,0 +1,45 @@
+#Sniper Bot 1.0
+
+**Este arquivo é para consulta**
+
+##TradingView Widget
+
+```<!-- TradingView Widget BEGIN -->
+  <div class="tradingview-widget-container">
+  <div id="tradingview_f658d"></div>
+  <div class="tradingview-widget-copyright">
+    <a
+      href="https://br.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE"
+      rel="noopener"
+      target="_blank"
+      ><span class="blue-text">Gráfico BTCUSDT</span></a
+    >
+    por TradingView
+  </div>
+  <script
+    type="text/javascript"
+    src="https://s3.tradingview.com/tv.js"
+  ></script>
+  <script type="text/javascript">
+    new TradingView.widget({
+      autosize: true,
+      symbol: 'BINANCE:BTCUSDT',
+      timezone: 'Etc/UTC',
+      theme: 'dark',
+      style: '1',
+      locale: 'br',
+      toolbar_bg: '#f1f3f6',
+      enable_publishing: true,
+      withdateranges: true,
+      range: 'ALL',
+      hide_side_toolbar: false,
+      allow_symbol_change: true,
+      details: true,
+      hotlist: true,
+      calendar: true,
+      container_id: 'tradingview_f658d',
+    })
+  </script>
+</div>
+<!-- TradingView Widget END -->
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import useWebSocket from 'react-use-websocket'
+import './App.css'
 
 function App() {
+  const [ticker, setTicker] = useState({})
+  const [tradingView, setTradingView] = useState({})
+  const { lastJsonMessage } = useWebSocket(
+    'wss://stream.binance.com:9443/stream?streams=btcusdt@ticker',
+    {
+      onMessage: () => {
+        if (lastJsonMessage && lastJsonMessage.data) {
+          if (lastJsonMessage.stream === 'btcusdt@ticker') {
+            setTicker(lastJsonMessage.data)
+          }
+        }
+      },
+      onError: (event) => {
+        alert(event)
+      },
+    }
+  )
+
+  useEffect(() => {
+    const tv = new window.TradingView.widget({
+      autosize: true,
+      symbol: 'BINANCE:BTCUSDT',
+      interval: '60',
+      timezone: 'Etc/UTC',
+      theme: 'dark',
+      style: '1',
+      locale: 'br',
+      toolbar_bg: '#f1f3f6',
+      enable_publishing: false,
+      hide_side_toolbar: false,
+      allow_symbol_change: true,
+      details: true,
+      container_id: 'tradingview_66414',
+    })
+    setTradingView(tv)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="">
+      <h2>SniperBot 1.0</h2>
+      <div class="tradingview-widget-container">
+        <div id="tradingview_66414"></div>
+      </div>
+      <div>
+        <div>
+          <b>Ticker 24H</b>
+          <br />
+          Open:{ticker && ticker.o} <br />
+          Hight:{ticker && ticker.h} <br />
+          Low:{ticker && ticker.l} <br />
+          Last:{ticker && ticker.c} <br />
+          Change %:{ticker && ticker.P} <br />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
